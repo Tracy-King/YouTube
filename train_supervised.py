438dbdf45fa54e648562cccdd4bd134b68fafacb
Ruidong Jin <sjtu.tracyking@gmail.com>
2022-09-26 15:44:24 +0900

    9.26-3

--- train_supervised.py
+++ train_supervised.py
@@ -281,7 +281,7 @@ for i in range(args.n_runs):
       labels_batch = train_data.labels[s_idx: e_idx]
 
       size = len(sources_batch)
-      pred_prob_num = torch.zeros((N_DECODERS, size, 2)).to(device)
+      pred_prob_num = torch.zeros((N_DECODERS, size, 1)).to(device)
 
 
       source_embedding = tgn.compute_temporal_embeddings(sources_batch,
@@ -290,11 +290,11 @@ for i in range(args.n_runs):
                                                                                      edge_idxs_batch,
                                                                                      NUM_NEIGHBORS)
 
-      labels_batch_torch = torch.from_numpy(labels_batch).long().to(device)
-      with torch.no_grad():
-        ones = torch.sparse.torch.eye(2)
-        labels_batch_onehot = ones.index_select(0, torch.from_numpy(labels_batch)).to(device)
-      #labels_batch_torch = torch.from_numpy(labels_batch).float().to(device)
+      #labels_batch_torch = torch.from_numpy(labels_batch).long().to(device)
+      #with torch.no_grad():
+      #  ones = torch.sparse.torch.eye(2)
+      #  labels_batch_onehot = ones.index_select(0, torch.from_numpy(labels_batch)).to(device)
+      labels_batch_torch = torch.from_numpy(labels_batch).float().to(device)
       '''
       weight = torch.from_numpy(np.array([1.0 if i==0 else 10.0 for i in labels_batch]).astype(np.float32)).to(device)
       decoder_loss_criterion = torch.nn.BCELoss(weight=weight)
@@ -327,7 +327,7 @@ for i in range(args.n_runs):
           neg_count = size - pos_count
           pos_weight = neg_count / (pos_count + 1)
           #print("pos_weight:{}".format(pos_weight))
-
+          '''
           # under sampling start
           index = list(range(size))
           sample_pos_index = []
@@ -343,7 +343,7 @@ for i in range(args.n_runs):
           random.shuffle(sample_pos_index)
           sample_index = sample_pos_index
           #under sampling end
-
+          '''
           #train_x = source_embedding[sample_index].clone().detach().cpu().numpy()
           #train_y = labels_batch[sample_index]
           #train_x = source_embedding.clone().detach().cpu().numpy()
@@ -351,7 +351,7 @@ for i in range(args.n_runs):
           #print(len(sample_index))
               # under sampling end
           #pred_u = pred[sample_index].clone().detach().cpu().numpy()
-          #decoder_loss_criterion = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor(pos_weight).to(device))
+          decoder_loss_criterion = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor(pos_weight).to(device))
           #if DECODER=='GBDT':
           #  decoder.fit(train_x, train_y)   
           #elif DECODER=='XGB':
@@ -364,8 +364,8 @@ for i in range(args.n_runs):
           for d_idx in range(N_DECODERS):
             pred_prob_num[d_idx] = decoders[d_idx](source_embedding)
           pred = torch.mean(pred_prob_num, 0).squeeze()
-          #pred_label = (pred + 0.5).trunc().detach().cpu().numpy()
-          pred_label = torch.argmax(pred, dim=1).detach().cpu().numpy()
+          pred_label = (pred + 0.5).trunc().detach().cpu().numpy()
+          #pred_label = torch.argmax(pred, dim=1).detach().cpu().numpy()
 
           if len(np.unique(labels_batch)) == 2:
             #print(pred_label.shape, labels_batch.shape)
@@ -373,7 +373,8 @@ for i in range(args.n_runs):
             train_pre = precision_score(labels_batch, pred_label)
           #print(pred.shape)
           #pred = (pred_prob_num+0.5).trunc()
-          decoder_loss = decoder_loss_criterion(pred[sample_index], labels_batch_onehot[sample_index])
+          decoder_loss = decoder_loss_criterion(pred, labels_batch_torch)
+          #decoder_loss = decoder_loss_criterion(pred[sample_index], labels_batch_onehot[sample_index])
       else:
           for d_idx in range(N_DECODERS):
               pred_prob_num[d_idx] = decoders[d_idx](source_embedding)
